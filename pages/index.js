2c1360fb338db09a1c801a9076997243beb2afaf
Paul Reichelt <paul.reichelt@outlook.com>
2020-10-30 17:34:04 +0100

    Add next

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+/**
+ * Start Page
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function Index() {
+  return <div>Open Kegelbillard</div>
+}
+
+export default Index
