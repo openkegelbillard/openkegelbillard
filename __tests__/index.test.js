684c87793b6f8cb95d0bd8c991ca1a25af704a6c
Paul Reichelt <paul.reichelt@outlook.com>
2020-10-30 19:30:57 +0100

    Setup project (#1)

---
+++ __tests__/index.test.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+test('default', () => {
+  expect(1).toBeTruthy()
+})
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Typography } from '@material-ui/core'
+
+/**
+ * Start Page
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function Index() {
+  return <Typography>Open Kegelbillard</Typography>
+}
+
+export default Index
